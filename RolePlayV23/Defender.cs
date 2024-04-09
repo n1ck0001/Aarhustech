07fee9afca7d7668ccf14a6dc74e467b145e20b7
NP <np@skoleit.dk>
2024-04-09 09:46:34 +0200

    Added RolePlayV23

---
+++ RolePlayV23/Defender.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace RolePlayV23
+{
+    public class Defender : Character
+    {
+        public Defender(string name, int hitPoints, int minDamage, int maxDamage) : base(name, hitPoints, minDamage, maxDamage)
+        { 
+             
+        }
+
+        protected override int ReceiveDamageModifyChance
+        {
+            get { return 45; }
+        }
+
+        protected override int CalculateModifiedReceivedDamage(int receivedDamage)
+        {
+            var originalDamage = base.CalculateModifiedReceivedDamage(receivedDamage);
+            var damageTaken = base.CalculateModifiedReceivedDamage(receivedDamage)/2;
+            //Console.WriteLine(originalDamage+"Original damage");
+            //Console.WriteLine(damageTaken+"damage taken as defender");
+            return damageTaken;
+
+        }
+    }
+}
---
+++ RolePlayV23/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using RolePlayV23;
+
+CharacterGroup redTeam = new CharacterGroup("Team Red");
+//redTeam.AddCharacter(new Character("Angor", 100, 15, 25));
+redTeam.AddCharacter(new Character("Zurin", 85, 18, 30));
+redTeam.AddCharacter(new Defender("RedCha1", 1185, 18, 30));
+
+CharacterGroup greenTeam = new CharacterGroup("Team Green");
+//greenTeam.AddCharacter(new Character("Baldur", 120, 12, 18));
+greenTeam.AddCharacter(new Character("Eliza", 80, 20, 35));
+greenTeam.AddCharacter(new Defender("GreenCha1", 1185, 18, 30));
+BattleHandler.DoBattle(redTeam, greenTeam);
