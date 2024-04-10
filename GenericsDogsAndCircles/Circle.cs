9c9f76187d756bc7cf14d746c282b76aa491cdc7
NP <np@skoleit.dk>
2024-04-10 11:36:44 +0200

    Added GenericDogsAndCircles

---
+++ GenericsDogsAndCircles/Circle.cs
@@ -0,0 +1,26 @@
+﻿
+public class Circle
+{
+    #region Properties
+    public double Radius { get; }
+    public double X { get; }
+    public double Y { get; }
+    public double Area { get { return Math.PI * Radius * Radius; } }
+    #endregion
+    
+    #region Constructor
+    public Circle(double radius, double x, double y)
+    {
+        Radius = radius;
+        X = x;
+        Y = y;
+    }
+    #endregion
+
+    #region Methods
+    public override string ToString()
+    {
+        return $"Circle at ({X} , {Y}) has an area of {Area:#.000000}";
+    }
+    #endregion
+}
---
+++ GenericsDogsAndCircles/Dog.cs
@@ -0,0 +1,25 @@
+﻿
+public class Dog
+{
+    #region Properties
+    public string Name { get; }
+    public int Height { get; }
+    public double Weight { get; }
+    #endregion
+
+    #region Constructor
+    public Dog(string name, int height, double weight)
+    {
+        Name = name;
+        Height = height;
+        Weight = weight;
+    }
+    #endregion
+
+    #region Methods
+    public override string ToString()
+    {
+        return $"{Name} is {Height} cm tall, and weighs {Weight} kgs.";
+    }
+    #endregion
+}
