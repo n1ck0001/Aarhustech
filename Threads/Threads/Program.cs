e4eec3dd57d24b9a4485af01f3fed6e2a60c9130
NP <np@skoleit.dk>
2024-04-16 12:35:04 +0200

    Done with part 1 and 2 of Threads

--- Threads/Threads/Program.cs
+++ Threads/Threads/Program.cs
@@ -4,6 +4,7 @@ Console.WriteLine("Hello, World!");
 
 int id = 5;
 int id2 = 2;
+int Increment = 0;
 
 string threadNUmber1 = "Thread number 1";
 string threadNUmber = "Thread number 2";
@@ -11,7 +12,7 @@ string threadNUmber = "Thread number 2";
 
 Thread newThread = new Thread(() => ThreadMethod(id, threadNUmber1));
 
-Thread newThread2 = new Thread(() => ThreadMethod(id2,threadNUmber));
+Thread newThread2 = new Thread(() => ThreadMethod(id2, threadNUmber));
 newThread.Start();
 newThread2.Start();
 
@@ -22,19 +23,51 @@ newThread2.Join();
 
 Console.WriteLine("Done");
 
-Console.ReadLine();
 
-static void ThreadMethod(int id,string message)
+static void ThreadMethod(int id, string message)
 {
     for (int i = 0; i < 10; i++)
     {
         Console.WriteLine($"{i} {id}, {message}");
-
     }
+}
+object lockObject = new object();
 
+Thread CounterThread = new Thread(() => IncrementThread());
+
+Thread CounterThread2 = new Thread(() => IncrementThread2());
+
+CounterThread.Start();
+CounterThread2.Start();
+
+CounterThread2.Join();
+CounterThread.Join();
+
+
+
+void IncrementThread()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        lock (lockObject) 
+        {
+            Increment++;
+            Console.WriteLine($"Incremented to {Increment}");
+        }
+        Thread.Sleep(100);  
+    }
+}
+void IncrementThread2()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        lock (lockObject)  
+        {
+            Console.WriteLine($"Read by Thread 2: {Increment}");
+        }
+        Thread.Sleep(100);  
+    }
 }
 
 
 
-
-    
