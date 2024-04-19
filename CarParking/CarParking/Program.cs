dd5b0df8076785da8ad543db71e4c770d5545279
NP <np@skoleit.dk>
2024-04-19 10:39:06 +0200

    make car parking more readable

--- CarParking/CarParking/Program.cs
+++ CarParking/CarParking/Program.cs
@@ -6,17 +6,23 @@ Console.WriteLine("Creating cars");
 
 var parkingLot = new ParkingLot { Name = "ParkingLot1", TotalSpaces = 3, ParkedCars= new List<Car>() };
 var parkingService = new ParkingService();
-var car1 = new Car { Model = "Model1", ParkingTime = 1000000, Color = "Red" , QueTime = 3000};
-var car2 = new Car { Model = "Model2", ParkingTime = 35162, Color = "Blue" , QueTime = 7000};
-var car3 = new Car { Model = "Model3", ParkingTime = 2742, Color = "Yellow", QueTime = 34523 };
-var car4 = new Car { Model = "Model4", ParkingTime = 50000, Color = "Green", QueTime = 16135 };
-
-
 var ActiveCars = new List<Car>();
-ActiveCars.Add(car1);
-ActiveCars.Add(car2);
-ActiveCars.Add(car3);
-ActiveCars.Add(car4);
+async Task CreateCars()
+{
+    var car1 = new Car { Model = "Model1", ParkingTime = 1000000, Color = "Red", QueTime = 3000 };
+    var car2 = new Car { Model = "Model2", ParkingTime = 35162, Color = "Blue", QueTime = 7000 };
+    var car3 = new Car { Model = "Model3", ParkingTime = 2742, Color = "Yellow", QueTime = 34523 };
+    var car4 = new Car { Model = "Model4", ParkingTime = 50000, Color = "Green", QueTime = 16135 };
+
+
+
+    ActiveCars.Add(car1);
+    ActiveCars.Add(car2);
+    ActiveCars.Add(car3);
+    ActiveCars.Add(car4);
+}
+await CreateCars().ConfigureAwait(false);
+
 Dictionary<Task, Car> tasksForParkedCards = new Dictionary<Task, Car>();
 Dictionary<Task,Car> tasks = new Dictionary<Task,Car>();
 foreach (var car in ActiveCars)
@@ -33,19 +39,27 @@ while (true)
         {
             var car = tasks[completedTask];
             tasks.Remove(completedTask);
+            Console.ForegroundColor= ConsoleColor.Yellow;
             Console.WriteLine($"Car is requesting parking {car.Model}");
+            Console.ForegroundColor = ConsoleColor.White;
             var granted = await parkingService.RequestParkingAsync(parkingLot);
             if (granted)
             {
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine($"Parking granted for {car.Model}. Parking now...");
+                Console.ForegroundColor = ConsoleColor.White;
                 await parkingService.ParkCarAsync(car, parkingLot);
                 var parkTask = parkingService.CarIsCurrentlyParked(car.ParkingTime);
                 tasksForParkedCards.Add(parkTask, car);
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
                 Console.WriteLine($"Car was parked successfully {car.Model}");
+                Console.ForegroundColor = ConsoleColor.White;
             }
             else
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine($"Parking denied for {car.Model}. Requeuing...");
+                Console.ForegroundColor = ConsoleColor.Yellow;
                 var newTask = parkingService.QueCarAsync(car.QueTime);
                 tasks.Add(newTask, car);
             }
@@ -54,7 +68,9 @@ while (true)
         {
             var car = tasksForParkedCards[completedTask];
             tasksForParkedCards.Remove(completedTask);
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine($"Car is leaving lot {car.Model}");
+            Console.ForegroundColor = ConsoleColor.White;
             await parkingService.LeaveParkingLot(parkingLot, car);
             var queueTask = parkingService.QueCarAsync(car.QueTime);
             tasks.Add(queueTask, car);
@@ -66,6 +82,4 @@ while (true)
     }
 }
   
-
-
 
